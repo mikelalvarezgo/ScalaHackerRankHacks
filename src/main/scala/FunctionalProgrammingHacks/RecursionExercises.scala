bb808b42e82953cbe3db3c1875810541d266ce82
mikelalvarezgo <mikelalvarezgo@gmail.com>
2019-01-17 18:46:14 +0100

    Added more recursion exercises

---
+++ src/main/scala/FunctionalProgrammingHacks/RecursionExercises.scala
@@ -0,0 +1,79 @@
+package FunctionalProgrammingHacks
+
+import scala.annotation.tailrec
+
+/**
+  * Created by mikelalvarezgo on 15/01/2019.
+  */
+object RecursionExercises {
+
+
+  /*
+  StringReduces ----> https://www.hackerrank.com/challenges/string-reductions/problem
+   */
+
+  def stringReduces(string: String): String =
+    string.foldLeft("")((a: String, b: Char) => if (a.contains(b)) a else (a + b))
+
+
+  /*
+  String comprenssion -----> https://www.hackerrank.com/challenges/string-compression/problem
+   */
+
+  def stringCompression(string: String): String = {
+    @tailrec
+    def recCompression(count: Int, tail: String, accString: String): String = tail.length match {
+      case 0 => accString
+      case 1 => accString + tail.head
+      case _ =>
+        if (tail.head == tail.tail.head)
+          recCompression(count + 1, tail.tail, accString)
+        else {
+          val newAccString = if (count != 1) s"$accString${tail.head}" else s"$accString${tail.head}"
+          recCompression(1, tail.tail, newAccString)
+        }
+    }
+    recCompression(1,string,"")
+  }
+
+
+
+  /*
+  Filter elements : https://www.hackerrank.com/challenges/filter-elements/problem
+
+  Given a list of N integers A = [a1, a2, ..., aN], you have to find those integers which are repeated at least K times. In case no such element exists you have to print -1.
+
+  If there are multiple elements in A which are repeated at least K times, then print these elements ordered by their first occurrence in the list.
+   */
+
+  def filterOnly[A](l:List[A], k:Int) = {
+    @tailrec
+    def recFilterOnly(l:List[A], k:Int, filtered: List[A]):List[A] = l match {
+      case Nil => filtered
+      case head :: Nil => if (k == 1 && ! filtered.contains(head)) filtered :+ head else filtered
+      case head :: tail =>
+        if ((tail.filter(_ == head).size >= k-1) && (!filtered.contains(head))) recFilterOnly(tail,k, filtered :+ head)
+        else  recFilterOnly(tail,k,filtered)
+
+    }
+    recFilterOnly(l,k,Nil)
+
+  }
+
+  /*
+  https://www.hackerrank.com/challenges/super-digit/problem
+
+   */
+
+  def superDigit(l: Int, k: Int) = {
+    val extendedInt = (1 to k).toList.foldLeft("")((a,b) => a ++ l.toString)
+
+    def recSuperDigitCalc(i:Int):Int = i match  {
+      case d:Int if (d < 10) => d
+      case d:Int => recSuperDigitCalc(d.toString.foldLeft{0}{(a,b) => a + (b.toString.toInt)})
+
+    }
+    recSuperDigitCalc(extendedInt.toInt)
+  }
+
+}
---
+++ src/test/scala/FunctionalProgrammingHacks/RecursionExercicesTest.scala
@@ -0,0 +1,48 @@
+package FunctionalProgrammingHacks
+package test
+import org.scalatest.{FlatSpec, Matchers}
+import RecursionExercises._
+
+/**
+  * Created by mikelalvarezgo on 15/01/2019.
+  */
+class RecursionExercicesTest extends FlatSpec with Matchers {
+
+  "String reduces function" should  "return expected result" in {
+
+    stringReduces("aaaaaabbbbbbbccccc") shouldBe   "abc"
+
+    stringReduces("aerererere") shouldBe "aer"
+
+    stringReduces("  ") shouldBe " "
+
+    stringReduces("Hello World!") shouldBe "Helo Wrd!"
+   }
+
+  "String compression function" should  "return expected result" in {
+
+    stringCompression("aaabccdd") shouldBe "abcd"
+
+    stringCompression("faceeface") shouldBe "faceface"
+
+    stringCompression("") shouldBe ""
+
+    stringCompression("a") shouldBe "a"
+  }
+
+  "Filtered members repeated less than K times" should "return expected result for any type" in {
+
+    val list1 = List(1,1,1,2,2,3,3,3,4,4,5,5,5)
+    filterOnly[Int](list1,3) shouldBe List(1,3,5)
+
+    val list2 = List('a', 'a', 'a', 'b','b', 'c','c', 'c')
+    filterOnly[Char](list2,3) shouldBe List('a','c')
+  }
+
+  "Super digits" should "return the expected results" in {
+
+    superDigit(148,3) shouldBe 3
+
+  }
+
+}
