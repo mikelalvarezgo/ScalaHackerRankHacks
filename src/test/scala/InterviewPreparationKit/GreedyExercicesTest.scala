640394896428f10117fbcfc17f25cc1408a163d8
Mikel Alvarez <mikelalvarezgo@gmail.com>
2022-06-29 18:13:46 +0200

    Added greedy algorithms exercises

---
+++ src/test/scala/InterviewPreparationKit/GreedyExercicesTest.scala
@@ -0,0 +1,19 @@
+package InterviewPreparationKit
+
+import InterviewPreparationKit.Greedy._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
+
+final class GreedyExercicesTest extends AsyncWordSpec with Matchers {
+
+  "Minimum absolute difference function" should {
+    "return expected result" in {
+      minimumAbsoluteDifference(Array(3 ,-7 ,0)) shouldBe 3
+    }
+    "Luck balance function" should {
+      "return expected result" in {
+        luckBalance(2, Array(Array(5, 1), Array(1, 1), Array(4, 0))) shouldBe 10
+      }
+    }
+  }
+}
